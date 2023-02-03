433f7f0f80c47f933acef0c27945fe245c5303ef
raochongwei <raochongwei@whty.com.cn>
2023-02-03 15:41:54 +0800

    feat: 添加登录验证 处理upgradeHandler

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,24 @@
 const User = require('../models/user')
+const uuid = require('uuid')
 
 // 获取账户详情
 function userLogin(req, res, next) {
+  const session = uuid.v4();
   const user = {
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
   }
+
   User
     .findOne({ ...user })
     .exec(function (err, data) {
       if (err) { return next(err) }
 
       if (data) {
+        // set-session
+        req.session.userId = session
+        req.session.userName = data.username
+
         res.send({ code: 0, data })
       } else {
         // Member.init()
@@ -20,8 +27,11 @@ function userLogin(req, res, next) {
         instence
           .save(function(createErr, user) {
             if (createErr) return next(createErr)
-            res.send({code: 0, data: user})
+            // set-session
+            req.session.userId = session
+            req.session.userName = user.username
 
+            res.send({code: 0, data: user})
           })
       }
     });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const http = require('http')
 const UserRouter = require('./routes/user')
 const mongoose = require('mongoose')
@@ -6,8 +7,10 @@ const WebSocket = require('ws')
 const WebSocketServer = WebSocket.Server
 
 const app = express()
+const map = new Map()
 // 设置默认 mongoose 连接
 const mongoDB = 'mongodb://127.0.0.1/admin';
+mongoose.set('strictQuery', false);
 mongoose.connect(mongoDB);
 // 让 mongoose 使用全局 Promise 库
 mongoose.Promise = global.Promise;
@@ -17,62 +20,104 @@ const db = mongoose.connection;
 // 将连接与错误事件绑定（以获得连接错误的提示）
 db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
 
+// 创建session-parse
+const sessionParser = session({
+  saveUninitialized: false,
+  secret: '$eCuRiTy',
+  resave: false
+});
+
+// 中间件
+app.use(sessionParser);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', UserRouter)
-app.set('port', 8020)
 
+// 路由
+app.use('/api', UserRouter)
+
+//端口
+app.set('port', 8010)
+
+// 创建serve实例
 const server = http.createServer(app)
 
-server.listen(8020)
+// 创建 websocket 服务器
+const wss = new WebSocketServer({ clientTracking: false, noServer: true });
 
-// 创建 websocket 服务器 监听在 3000 端口
-const wss = new WebSocketServer({port: 8010})
+// 监听upgrade
+server.on('upgrade', function (request, socket, head) {
+
+  socket.on('error', onSocketError);
+
+  console.log('Parsing session from request...');
+
+  // 解析session
+  sessionParser(request, {}, () => {
+    if (!request.session) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+
+    console.log('Session is parsed!');
+
+    socket.removeListener('error', onSocketError);
+
+    // 触发websocket的链接
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+});
 
 // 服务器被客户端连接
 wss.on('connection', (ws, req) => {
-  const test = {
+  const userId = req.session.userId;
+  const userName = req.session.userName;
+
+  const notification = {
     type: 'text',
     data: {
-      id: 0,
+      id: userId,
       type: 0,
-      name: '',
-      msg: '欢迎‘阿婆’加入聊天'
+      name: 'System',
+      msg: `欢迎${userName}加入群聊`
     }
   }
 
-  const systemMsg = JSON.stringify(test)
-  
-  ws.send(systemMsg, (err) => {
-    if (err) {
-      console.log(`[SERVER] error: ${err}`);
-    }
-  })
+  ws.send(JSON.stringify(notification))
+
+  map.set(userId, ws);
+
+  ws.on('error', console.error);
 
   ws.on('message', (message) => {
-    const msg = message.toString()
 
+    const msg = message.toString()
     const response = JSON.stringify({
-      type: 'text',
-      data: {
-        id: 0,
-        type: 1,
-        name: 'Grece',
-        msg
-      }
-    })
+        type: 'text',
+        data: {
+          id: userId,
+          type: 1,
+          name: userName,
+          msg
+        }
+      })
 
     ws.send(response, (err) => { // send 方法的第二个参数是一个错误回调函数
       if (err) {
         console.log(`[SERVER] error: ${err}`);
       }
     })
+
+    ws.on('close', function () {
+      map.delete(userId);
+    });
   })
 })
 
-wss.on('wsClientError', (err, ws, req) => {
-  console.log('client-error')
-  console.log(error)
-})
+function onSocketError(err) {
+  console.error(err);
+}
 
-wss.on('error',(error) => console.log('error' + error) )
+server.listen(8010)
