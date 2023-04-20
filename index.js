d53785f9a12a2d8995b77d954994716681427795
raochongwei <raochongwei@whty.com.cn>
2023-04-20 17:22:15 +0800

    增加连接逻辑

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const session = require('express-session')
 const http = require('http')
 const UserRouter = require('./routes/user')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const WebSocket = require('ws')
 const WebSocketServer = WebSocket.Server
 
 const app = express()
-const map = new Map()
-// 设置默认 mongoose 连接
-const mongoDB = 'mongodb://127.0.0.1/admin';
-mongoose.set('strictQuery', false);
-mongoose.connect(mongoDB);
-// 让 mongoose 使用全局 Promise 库
-mongoose.Promise = global.Promise;
-// 取得默认连接
-const db = mongoose.connection;
+map = new Map()
+const cacheChannel = new Map()
+// // 设置默认 mongoose 连接
+// const mongoDB = 'mongodb://127.0.0.1/admin';
+// mongoose.set('strictQuery', false);
+// mongoose.connect(mongoDB);
+// // 让 mongoose 使用全局 Promise 库
+// mongoose.Promise = global.Promise;
+// // 取得默认连接
+// const db = mongoose.connection;
 
-// 将连接与错误事件绑定（以获得连接错误的提示）
-db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
+// // 将连接与错误事件绑定（以获得连接错误的提示）
+// db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
 
 // 创建session-parse
 const sessionParser = session({
@@ -42,14 +43,12 @@ app.set('port', 8010)
 const server = http.createServer(app)
 
 // 创建 websocket 服务器
-const wss = new WebSocketServer({ clientTracking: false, noServer: true });
+const wss = new WebSocketServer({ clientTracking: true, noServer: true });
 
 // 监听upgrade
 server.on('upgrade', function (request, socket, head) {
 
   socket.on('error', onSocketError);
-
-  console.log('Parsing session from request...');
 
   // 解析session
   sessionParser(request, {}, () => {
@@ -58,8 +57,6 @@ server.on('upgrade', function (request, socket, head) {
       socket.destroy();
       return;
     }
-
-    console.log('Session is parsed!');
 
     socket.removeListener('error', onSocketError);
 
@@ -72,47 +69,92 @@ server.on('upgrade', function (request, socket, head) {
 
 // 服务器被客户端连接
 wss.on('connection', (ws, req) => {
-  const userId = req.session.userId;
+
+  const userId = req.session.userId
   const userName = req.session.userName;
 
-  const notification = {
-    type: 'text',
-    data: {
-      id: userId,
-      type: 0,
-      name: 'System',
-      msg: `欢迎${userName}加入群聊`
-    }
-  }
+  // 给当前链接的socket添加id
+  ws.connectionId = req.session.userId
 
-  ws.send(JSON.stringify(notification))
+  // 存入user-map
+  map.set(userId, userName);
 
-  map.set(userId, ws);
-
-  ws.on('error', console.error);
+  // 发送一条系统消息
+  wss.clients.forEach(client => {
+    client.send(JSON.stringify({
+      type: 'system',
+      data: {
+        msg: `欢迎${userName}加入群聊`
+      }
+    }))
+  })
 
   ws.on('message', (message) => {
-
     const msg = message.toString()
-    const response = JSON.stringify({
-        type: 'text',
-        data: {
-          id: userId,
-          type: 1,
-          name: userName,
-          msg
-        }
-      })
+    const { type, data } = JSON.parse(msg)
 
-    ws.send(response, (err) => { // send 方法的第二个参数是一个错误回调函数
-      if (err) {
-        console.log(`[SERVER] error: ${err}`);
+    wss.clients.forEach(client => {
+      switch (type) {
+        case 'offer':
+          if(client !== ws) {
+            client.send(msg)
+          }
+          break;
+
+        case 'answer':
+          if(client !== ws) {
+            client.send(msg)
+          }
+          break;
+        
+        case 'connect':
+          if(data === client.connectionId) {
+            client.send(JSON.stringify({
+              type: 'connect',
+              data: {
+                reqId: ws.connectionId,
+                reqName: map.get(ws.connectionId)
+              }
+            }))
+          }
+          
+          break;
+        case 'accept':
+          if (client !== ws) {
+            client.send(msg)
+          }
+          break;
+
+        case 'denied':
+          if (client !== ws) {
+            client.send(msg)
+          }
+          break;
+
+        default:
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({
+              type,
+              data: {
+                id: client.connectionId,
+                self: client === ws,
+                name: map.get(client.connectionId),
+                msg: data
+              }
+            }))
+          }
+          break;
       }
     })
 
+    // 关闭链接
     ws.on('close', function () {
+      console.log('c')
       map.delete(userId);
     });
+
+    // 错误链接
+    ws.on('error', console.error);
   })
 })
 
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,23 @@ const Users = require('../controllers/user')
 
 /* GET users listing. */
 router.post('/login', Users.userLogin);
+router.get('/group', (req, res, next) => {
+
+  let data = []
+  for (const [key, value] of global.map) {
+    console.log(`${key} = ${value}`);
+    data.push({
+      id: key,
+      name: value
+    })
+  }
+
+  res.send(
+    {
+      code: 0,
+      data
+    }
+  )
+})
 
 module.exports = router;
